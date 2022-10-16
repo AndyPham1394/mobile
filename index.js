b641d760c02dad2815f419b4f8d27a3813dbcc37
Pham Van Dung <dungpv2@vmodev.com>
2022-10-16 23:24:34 +0700

    add connection counting

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+var connectionCount = 0;
 
 app.get('/', (req,res) => {
-    res.send('mobile app');
+    console.log('new connection from IP: ' + req.ip);
+    connectionCount++;
+    res.send(`mobile app - connection count: ${connectionCount}`);
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
